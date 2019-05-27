f834d574d49a80d7267b90fb9406a71be8beb6e6
Dinaerte Neto <dinaerteneto@gmail.com>
2019-05-26 21:02:54 -0300

    event guest - form guest

--- app/routes/web.php
+++ app/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', function () {return view('auth/login');});
 Route::get('/cadastrar', function () {return view('auth/register');});
 Route::post('/cadastrar', 'HomeController@register');
 
-Route::get('/event/{eventId}/{eventStart}/{eventName}', 'EventController@page');
+Route::get('/event/{eventId}/{eventStart}/{eventName}', 'EventController@page')->where('eventId', '[0-9]+');;
 Route::post('/event/{eventId}/page', 'EventController@page');
 
 Route::group(['middleware' => ['web', 'auth']], function () {
