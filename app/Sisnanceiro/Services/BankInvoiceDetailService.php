3eed25a2a531a67b3ab1e88cc016902253480350
Dinaerte Neto <dinaerteneto@gmail.com>
2020-07-26 10:51:51 -0300

    feat: add getReceiveDate

--- app/Sisnanceiro/Services/BankInvoiceDetailService.php
+++ app/Sisnanceiro/Services/BankInvoiceDetailService.php
@@ -9,43 +9,61 @@ use Sisnanceiro\Repositories\BankInvoiceDetailRepository;
 class BankInvoiceDetailService extends Service
 {
 
-    protected $rules = [
-        'create' => [
-            'bank_category_id'            => 'required',
-            'bank_account_id'             => 'required',
-            'bank_invoice_transaction_id' => 'required',
-            'gross_value'                 => 'required|numeric',
-            'net_value'                   => 'required|numeric',
-            'parcel_number'               => 'required',
-            'due_date'                    => 'required',
-            // 'status'                      => 'required',
-        ],
-        'update' => [
-            'bank_category_id'            => 'required',
-            // 'bank_account_id'             => 'required',
-            'bank_invoice_transaction_id' => 'required',
-            // 'gross_value'                 => 'required|numeric',
-            // 'net_value'                   => 'required|numeric',
-            // 'parcel_number'               => 'required',
-            // 'due_date'                    => 'required',
-            // 'status'                      => 'required',
-        ],
-    ];
+ protected $rules = [
+  'create' => [
+   'bank_category_id'            => 'required',
+   'bank_account_id'             => 'required',
+   'bank_invoice_transaction_id' => 'required',
+   'gross_value'                 => 'required|numeric',
+   'net_value'                   => 'required|numeric',
+   'parcel_number'               => 'required',
+   'due_date'                    => 'required',
+   // 'status'                      => 'required',
+  ],
+  'update' => [
+   'bank_category_id'            => 'required',
+   // 'bank_account_id'             => 'required',
+    'bank_invoice_transaction_id' => 'required',
+   // 'gross_value'                 => 'required|numeric',
+   // 'net_value'                   => 'required|numeric',
+   // 'parcel_number'               => 'required',
+   // 'due_date'                    => 'required',
+   // 'status'                      => 'required',
+  ],
+ ];
 
-    public function __construct(Validator $validator, BankInvoiceDetailRepository $repository)
-    {
-        $this->validator  = $validator;
-        $this->repository = $repository;
-    }
+ public function __construct(Validator $validator, BankInvoiceDetailRepository $repository)
+ {
+  $this->validator  = $validator;
+  $this->repository = $repository;
+ }
 
-    /**
-     * return BankInvoice of the inicial balance
-     * @param BankAccount $bankAccount
-     * @return BankInvoiceDetail
-     */
-    public function findInitialBalance(BankAccount $bankAccount)
-    {
-        return $this->repository->findInitialBalance($bankAccount);
-    }
+ /**
+  * recalcule receive date based payment method
+  * @param float $dueDate
+  * @return float
+  */
+ private function __getReceiveDate($dueDate)
+ {
+  return $dueDate;
+ }
+
+ public function store(array $data, $rules = false)
+ {
+  if ('create' == $rules) {
+   $data['receive_date'] = $this->__getReceiveDate($data['due_date']);
+  }
+  return parent::store($data, $rules);
+ }
+
+ /**
+  * return BankInvoice of the inicial balance
+  * @param BankAccount $bankAccount
+  * @return BankInvoiceDetail
+  */
+ public function findInitialBalance(BankAccount $bankAccount)
+ {
+  return $this->repository->findInitialBalance($bankAccount);
+ }
 
 }
