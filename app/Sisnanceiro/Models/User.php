ed3d07d2187fd93aec6c0d8a8f57b3dacba680c6
Dinaerte Neto <dinaerteneto@gmail.com>
2020-07-28 21:27:18 -0300

    feat: css text-blue

--- app/Sisnanceiro/Models/User.php
+++ app/Sisnanceiro/Models/User.php
@@ -5,33 +5,29 @@ namespace Sisnanceiro\Models;
 use App\Scopes\TenantModels;
 use Illuminate\Database\Eloquent\Model;
 
-class User extends Model
-{
-    use TenantModels;
+class User extends Model {
+	use TenantModels;
 
-    const GROUP_MASTER = 1;
-    const GROUP_ADMIN  = 2;
-    const GROUP_USER   = 3;
+	const GROUP_MASTER = 1;
+	const GROUP_ADMIN = 2;
+	const GROUP_USER = 3;
 
-    protected $fillable = [
-        'id', 'company_id', 'email', 'remember_token', 'password',
-    ];
+	protected $fillable = [
+		'id', 'company_id', 'email', 'remember_token', 'password',
+	];
 
-    protected $hidden = ['password'];
+	protected $hidden = ['password'];
 
-    public function username()
-    {
-        return 'login';
-    }
+	public function username() {
+		return 'login';
+	}
 
-    public function company()
-    {
-        return $this->hasOne('Sisnanceiro\Models\Company', 'id', 'company_id');
-    }
+	public function company() {
+		return $this->hasOne('Sisnanceiro\Models\Company', 'id', 'company_id');
+	}
 
-    public function person()
-    {
-        return $this->belongsTo('Sisnanceiro\Models\Person', 'id', 'id');
-    }
+	public function person() {
+		return $this->belongsTo('Sisnanceiro\Models\Person', 'id', 'id');
+	}
 
 }
